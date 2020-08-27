e0ff09b210f7f7da7087bd896d7912ad3899a767
monroekimmel <monroekimmel@gmail.com>
2020-08-26 21:39:34 -0400

    added user and vendors

--- prs-create-script.sql
+++ prs-create-script.sql
@@ -24,7 +24,8 @@ create table VENDOR (
     State			varchar(2)			not null,
     Zip				varchar(5)			not null,
     PhoneNumber		varchar(12)			not null,
-    Email			varchar(75)			not null
+    Email			varchar(75)			not null,
+    Constraint vcode unique (Code)
 );
 
 create table REQUEST (
@@ -62,3 +63,16 @@ create table LINEITEM (
     Foreign Key (RequestID) references request(ID),
     Constraint req_pdt unique (RequestID,ProductID)
 );
+
+insert into USER values
+	(1, 'SYSTEM', 'xxxxx', 'System', 'System', 'XXX-XXX-XXXX', 'system@test.com', 0, 0),
+    (2, 'monroekimmel', '030477', 'Monroe', 'Kimmel', '513-254-9374', 'monroekimmel@gmail.com', 0, 0)
+    ;
+
+insert into VENDOR values
+	(1, 'BB-1001', 'Best Buy', '100 Best Buy St', 'Louisville', 'KY', '40207', '502-111-9099', 'geeksquad@bestbuy.com'),
+    (2, 'AP-1001', 'Apple Inc', '1 Infinite Loop', 'Cupertino', 'CA', '95014', '800-123-4567', 'genius@apple.com'),
+    (3, 'AM-1001', 'Amazon', '410 Terry Ave N', 'Seattle', 'WA', '98109', '206-266-1000', 'amazon@amazon.com'),
+    (4, 'ST-1001', 'Staples', '9550 Mason Montgomery Rd', 'Mason', 'OH', '45040', '513-754-0235', 'support@orders.staples.com'),
+    (5, 'MC-1001', 'Micro Center', '11755 Mosteller Rd', 'Sharonville', 'OH', '45241', '513-782-8500', 'support@microcenter.com')
+    ;
